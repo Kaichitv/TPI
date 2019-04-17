4de42b2d1ea2fb60ce3f23d7137ba8f190b3d8e8
Jacot-dit-Montandon <31500192+Kaichitv@users.noreply.github.com>
2019-04-17 12:55:38 +0200

    Bases

---
+++ www/EUser.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @brief	Objet User
+ * @remark  Cet objet est utilisé comme conteneur
+ * 
+ *          Exemple d'utilisation 1
+ *          $u = new EUser();
+ *          $u->Email = "dominique@aigroz.com";
+ *          $u->Nickname = "Iraque";
+ *          $u->Name = "Dominique AIGROZ";
+ * 
+ *          Exemple d'utilisation 2
+ *          $u = new EUser("dominique@aigroz.com", "Iraque", "Dominique AIGROZ");
+ */
+class EUser {
+	/**
+	 * @brief	Le Constructor appelé au moment de la création de l'objet. Ie. new EUser();
+	 * @param InEmail		L'email de l'utilisateur. (Optionel) Defaut ""
+	 * @param InNickname	Le nickname de l'utilisateur. (Optionel) Defaut ""
+	 * @param InName	    Le nom complet de l'utilisateur. (Optionel) Defaut ""
+	  */
+    public function __construct($InEmail = "", $InNickname = "", $InName = "")
+    {
+		$this->Email = $InEmail;
+		$this->Nickname = $InNickname;
+        $this->Name = $InName;
+	}
+	/**
+	 * @var string L'email de l'utilisateur
+	 */
+	public $Email;
+	/**
+	 * @var string Le nickname de l'utilisateur
+	 */
+	public $Nickname;
+	/**
+	 * @var string Le nom complet de l'utilisateur
+	 */
+	public $Name;
+}
+
+
+
+?>
---
+++ www/functions.php
@@ -0,0 +1,88 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/TPI/www/db/database.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/TPI/www/EUser.php';
+
+/**
+ * @brief Retourne le tableau des utilisateurs de type EUser
+ * @return [array of EUser] Le tableau de EUser
+ */
+function GetAllUsers()
+{
+	// On crée un tableau qui va contenir les objets EUser
+	$arr = array();
+
+    $sql = "SELECT `EMAIL`, `NICKNAME`, `NAME` FROM `USERS`";
+	$request = EDatabase::prepare($sql,array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+	try {
+		$request->execute();
+	}
+	catch (PDOException $e) {
+        echo 'Problème de lecture de la base de données: '.$e->getMessage();
+		return false;
+	}
+	// On parcoure les enregistrements 
+	while ($row=$request->fetch(PDO::FETCH_ASSOC,PDO::FETCH_ORI_NEXT)){
+		// On crée l'objet EUser en l'initialisant avec les données provenant
+		// de la base de données
+		$u = new EUser($row['EMAIL'], $row['NICKNAME'], $row['NAME']);
+		// On place l'objet EUser créé dans le tableau
+		array_push($arr, $u);
+	}        
+	// On retourne le tableau contenant les utilisateurs sous forme EUser
+	return $arr;
+}
+
+function GetUserByEmail($user)
+{
+	$arr = array();
+
+	$sql = "SELECT `NICKNAME`, `NAME` FROM `USERS` WHERE `EMAIL`=:email";
+	$request = EDatabase::prepare($sql);
+	try{
+    $request->bindParam(":email", $user->email, PDO::PARAM_STR);
+    $request->execute();
+	}
+	catch (PDOException $e) {
+        echo 'Problème de lecture de la base de données: '.$e->getMessage();
+		return false;
+	}
+
+	return $arr;
+}
+
+function AddUser($user)
+{
+	$sql = "INSERT INTO `USERS` (`EMAIL`, `NICKNAME`, `NAME`)  VALUE (:email, :nickname, :name)";
+	$request = EDatabase::prepare($sql);
+	try{
+		$request->bindParam(":email", $user->email, PDO::PARAM_STR);
+		$request->bindParam(":nickname", $user->nickname, PDO::PARAM_STR);
+		$request->bindParam(":name", $user->name, PDO::PARAM_STR);
+		$request->execute();
+	}
+	catch (PDOException $e) {
+        echo 'Problème de lecture de la base de données: '.$e->getMessage();
+		return false;
+	}
+
+	return true;
+}
+
+function UpdateUser($user)
+{
+	$sql = "UPDATE `USERS` SET `NICKNAME`=:nickname, `NAME`=:name  WHERE `EMAIL`=:email";
+	$request = EDatabase::prepare($sql);
+	try{
+		$request->bindParam(":email", $user->email, PDO::PARAM_STR);
+		$request->execute();
+	}
+	catch (PDOException $e) {
+        echo 'Problème de lecture de la base de données: '.$e->getMessage();
+		return false;
+	}
+
+	return true;
+}
+
+
+?>
